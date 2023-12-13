fb58a607c1848d3c2d14568a1766c2b11a8c8b83
bookshelf <81616418+impulse-star@users.noreply.github.com>
2023-12-13 01:26:47 -0700

    added documentation for worlddata.js

--- worldData.js
+++ worldData.js
@@ -1,3 +1,8 @@
+/**
+ * A class that represents the data of the world at any given time.
+ * Data about the cameras position and the map should go here.
+ * Maybe a better name for this would be something like "GameState"
+ */
 export class WorldData {
     constructor() {
         this.map =
