4fa4dcc603fed88848c06d7d63e736adf56511cc
bookshelf <81616418+impulse-star@users.noreply.github.com>
2023-12-13 00:19:08 -0700

    Refactored some rendering code

--- index.js
+++ index.js
@@ -21,16 +21,17 @@ let cameraViewAngle = 0.0;
 function findGridPosition(cameraX, cameraY) {
     return { x: cameraX % BLOCK_SIZE, y: cameraY % BLOCK_SIZE };
 }
+function SetupBackground() {
+    // setup ceiling and floor
+    const canvasHeight = canvas.height;
+    const canvasWidth = canvas.width;
+    context.fillStyle = '#BFBFBF';
+    context.fillRect(0, 0, canvasWidth, canvasHeight / 2);
+    context.fillStyle = '#8F8F8F';
+    context.fillRect(0, canvasHeight / 2, canvasWidth, canvasHeight / 2);
+}
 
-
-
-// setup ceiling and floor
-const canvasHeight = canvas.height;
-const canvasWidth = canvas.width;
-context.fillStyle = '#BFBFBF';
-context.fillRect(0, 0, canvasWidth, canvasHeight / 2);
-context.fillStyle = '#8F8F8F';
-context.fillRect(0, canvasHeight / 2, canvasWidth, canvasHeight / 2);
+SetupBackground();
 
 // Runs Tests
 import('./test.js');
